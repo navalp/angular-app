9164703a4ffbd812d0aeb37269773cad429bf40b
naval patel <navalp@neuronsolutions.com>
2016-02-15 13:50:49 +0530

    removed components directory

--- app/components/auth/AuthModule.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hp on 2/4/2016.
- */
-(function () {
-    'use strict';
-
-    angular.module('AngularApp.Auth', ['ui.router']);
-
-})();
--- app/components/common/commonModule.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('AngularApp.Common', ['ui.router']);
-})();
-
